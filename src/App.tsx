06170e14f9cde3df331b40d5e703c078120dbd7a
Michal@SFB2 <michal.marusan@gmail.com>
2022-03-21 21:22:12 +0100

    handling responses & logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Stack, IStackStyles } from '@fluentui/react';
+import { Stack, IStackStyles, Link} from '@fluentui/react';
 // import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
-// import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 
@@ -13,8 +13,11 @@ import { IStackProps } from '@fluentui/react/lib/Stack';
 // import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
 import { PrimaryButton, ActionButton } from '@fluentui/react/lib/Button';
 import { IIconProps, initializeIcons } from '@fluentui/react';
+
 import { IStyleSet, Label, ILabelStyles, Pivot, PivotItem } from '@fluentui/react';
 
+
+initializeIcons();
 // const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
 
 const stackStyles: Partial<IStackStyles> = { root: { width: 650 } };
@@ -39,15 +42,41 @@ export const App: React.FunctionComponent = () => {
   const [uploading, setUploading] = useState(false);
   const [processed, setProcessed] = useState(false);
   const [processedDocument, setProcessedDocument] = useState(false);
-  const [translatedResults, setTranslatedResults] = useState<string>("");
-  const [translatedFiles, setTranslatedFiles] = useState(null);
+  const [translatedResults, setTranslatedResults] = useState<translateTextResponse>();
+  const [translatedFiles, setTranslatedFiles] = useState<translateDocResponse>();
 
   const addDownloadIcon: IIconProps = { iconName: 'Download' };
 
-  initializeIcons();
+  // initializeIcons();
 
   const sleep = (milliseconds: number) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+
+  // For the "unwrapping" variation
+
+
+  type translateTextResponse = {
+    translations: translateTextResponseRecord[];
+  }
+  type translateTextResponseRecord = {
+    text: string,
+    to: string
+  }
+  type translateDocResponse = {
+    fileurl: string,
+    original_filename: string,
+    translated_filename: string
+  }
+  function api<T>(url: string, requestOptions: RequestInit): Promise<T[]> {
+    return fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => data as T[])
+  }
+  function api2<T>(url: string, requestOptions: RequestInit): Promise<T> {
+    return fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => data as T)
   }
 
   const onTextChange = React.useCallback(
@@ -74,26 +103,27 @@ export const App: React.FunctionComponent = () => {
       redirect: 'follow'
     };
     
-    await fetch("/api/translate-text-api", requestOptions)
+    let data = await api<translateTextResponse>("/api/translate-text-api", requestOptions)
         .then((response) => {
-          if (!response.ok) {
-            throw new Error(
-              `This is an HTTP error: The status is ${response.status}`
-            );
-          }
-          return response.json();
+          // console.log("xxx")
+          // console.log(response);
+          return response;
           
         })
-        .then((data) => setTranslatedResults(data))
         .catch(error => console.log('error', error));
-    // console.log(JSON.stringify(translatedResults[0]))
+    // console.log("sssss")
+    // console.log(data)
+    if (data) {
+      // console.log(data[0]);
+      setTranslatedResults(data[0]);
+    }
+    
     setProcessed(true)
 
     // reset state/form
     setFileSelected(null);
     setUploading(false);
   }
-
   const onFileChange = React.useCallback(
     (event: React.FormEvent<HTMLInputElement>) => {
     setFileSelected(event.currentTarget.files?.item(0));
@@ -101,7 +131,7 @@ export const App: React.FunctionComponent = () => {
   },[],
   );
 
-  const onFileUpload = async () => {
+  const onFileUpload2 = async () => {
     // prepare UI
     setUploading(true);
     setProcessedDocument(false);
@@ -142,45 +172,88 @@ export const App: React.FunctionComponent = () => {
     setFileSelected(null);
     setUploading(false);
   };
+  const onFileUpload = async () => {
+      // prepare UI
+      setUploading(true);
+      setProcessedDocument(false);
+
+      // // *** UPLOAD TO AZURE STORAGE ***
+      // // const blobsInContainer = await uploadFileToBlob(fileSelected);
+      if (fileSelected){
+        // *** SEND FILE TO Azure Functions ***
+        var formdata = new FormData();
+        formdata.append("file", fileSelected, fileSelected.name);
+
+        var requestOptions : RequestInit = {
+          method: 'POST',
+          body: formdata,
+          redirect: 'follow'
+        };
 
 
+        let data = await api2<translateDocResponse>("/api/translate-doc-api", requestOptions)
+          .then((response) => {
+            // console.log("xxx")
+            // console.log(response);
+            return response;
+            
+          })
+          .catch(error => console.log('error', error));
+        
+        if (data) {
+          setTranslatedFiles(data)
+        }
+          
+    
+        console.log(translatedFiles)
+        await sleep(10*1000) //wait 10 seconds
+        
+        setProcessedDocument(true)
+      }
+      // reset state/form
+      setFileSelected(null);
+      setUploading(false);
+    };
 
+  function showTranslatedDocumentResult(translatedFiles: translateDocResponse | undefined): React.ReactNode {
+    return (
+      <Stack>
+        
+        {/* {translatedFiles?.translated_filename} */}
+        <ActionButton iconProps={addDownloadIcon}  href={translatedFiles?.fileurl} target="_blank" >stáhnout přeložený dokument</ActionButton>
+      </Stack>
+    )
+  }
+
+ 
+ 
   return (
     
     <Stack horizontalAlign="center" verticalAlign="start" verticalFill styles={stackStyles} tokens={stackTokens}>
-      {/* <img className="App-logo" src={logo} alt="logo" /> */}
+      <img className="App-logo" src={logo} alt="logo" />
       <Pivot aria-label="Basic Pivot Example">
-        <PivotItem
-          headerText="Překlad textu"
-          headerButtonProps={{
-            'data-order': 1,
-            'data-title': 'My Files Title',
-          }}
-        >
+        <PivotItem headerText="Překlad textu (UA->CZ)">
           <Stack {...columnProps}>
             <Label styles={labelStyles}>Text k překladu: (Text v poli níže můžete nahradit libovolným jiným textem)</Label>
             {/* Translation */}
             <TextField label="Váš text" multiline rows={3} value={text} onChange={onTextChange}/>
             <PrimaryButton text="Přeložit" allowDisabledFocus disabled={uploading} checked={false} onClick={onTranslate}/>
-            <TextField label="Překlad" multiline rows={3} disabled={processed} value={JSON.stringify(translatedResults[0])}/>
+            <TextField label="Překlad" multiline rows={3} disabled={!processed} value={translatedResults?.translations[0].text}/>
           </Stack>
         </PivotItem>
-        <PivotItem headerText="Přklad dokumentu">
+        <PivotItem headerText="Přklad dokumentu (CZ -> UA)">
           <Stack {...columnProps}>
             <Label styles={labelStyles}>Nahrajte soubor v CZ (*.docx, *.pdf)</Label>
             <input  name="file" type="file" onChange={onFileChange}  />
             <PrimaryButton text="Přeložit dokument" allowDisabledFocus disabled={uploading} checked={false} onClick={onFileUpload}/>
             {uploading? <ProgressIndicator label="Pracuji..." description="Nahrávám dokument a probíhá překlad." /> : null }
-            {processedDocument? <TextField label="Přeložený dokuemnt" multiline rows={3} value={JSON.stringify(translatedFiles)}/> :null}
-            {processedDocument? <ActionButton iconProps={addDownloadIcon}>stáhnout</ActionButton> : null}
-
-            {/* Přeložený dokument: 
-        {translatedFiles.fileurl}
-        &nbsp;|&nbsp;{translatedFiles.original_filename}
-        &nbsp;|&nbsp;{translatedFiles.translated_filename} */}
+            {processedDocument? showTranslatedDocumentResult(translatedFiles) :null}
           </Stack>
+          
         </PivotItem>
       </Pivot>
     </Stack>
   );
 };
+
+
